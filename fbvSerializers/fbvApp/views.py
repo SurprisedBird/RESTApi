08e571c2fc36969a2dcc3174831bb2cf8ebf90fb
SurprisedBird <irinayavors@gmail.com>
2021-03-10 15:24:13 +0200

    fixed errors in student_details

--- fbvSerializers/fbvApp/views.py
+++ fbvSerializers/fbvApp/views.py
@@ -20,6 +20,7 @@ def student_list(request):
             serializer.save()
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def student_details(request, pk):
@@ -30,9 +31,17 @@ def student_details(request, pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
         serializer = StudentSerializer(student, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
