711162d6325b85d189787e60596ce43ba1274ca3
SurprisedBird <irinayavors@gmail.com>
2021-04-08 11:20:32 +0300

    Use @api_view before student_list method

--- fbvSerializers/fbvApp/views.py
+++ fbvSerializers/fbvApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from fbvApp.models import Student
@@ -8,6 +9,7 @@ from fbvApp.serializers import StudentSerializer
 # Create your views here.
 
 
+@api_view(['GET', 'POST'])
 def student_list(request):
     if request.method == 'GET':
         students = Student.objects.all()
